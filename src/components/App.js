12bc5b6e71bbee3a06ba5c84628a89047d015dec
shashank <keshavashashank@gmail.com>
2017-08-07 23:30:48 +0530

    Working on filter

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,52 @@
 import React, { Component } from 'react';
 import './App.css';
+import BMDBHeader from './bmdbHeader.js';
+import BMDBImage from '../img/logo.png';
+import { Navbar, Image, NavItem, Nav, DropdownButton, MenuItem } from 'react-bootstrap';
+
+let initYear = 2010;
+const sortRating = ["Rating: Low", "Rating: High", "Popularity: Low", "Popularity: High"];
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    _sortRating = () => {
+        let sortItems = [];
+        sortItems = sortRating.map((item, index) => {
+            return (`<MenuItem id=${index}>${item}</MenuItem>`);
+        })
+        return (<DropdownButton title={"Sort By"} className={"BMDB-rating-filter"} id={"BMDB-rating-filter"}>
+            {sortItems}
+          </DropdownButton>);
+    }
+    _sortYear = () => {
+        let yearMenuItem = [];
+        while (initYear <= 2017) {
+            yearMenuItem.push(`<MenuItem id=${initYear}>${initYear}</MenuItem>`);
+            initYear++;
+        }
+        return (<DropdownButton title={"Sort By"} className={"BMDB-year-filter"} id={"BMDB-year-filter"}>
+                {yearMenuItem}
+                </DropdownButton>);
+    }
     render() {
         return (
-            <div className="App">
-              <div className="App-header">
-              <h2>Welcome to React</h2>
-              </div>
-              <p className="App-intro">
-              To get started, edit <code>src/App.js</code> and save to reload.
-              </p>
+            <div>
+              <Navbar>
+              <Navbar.Header>
+              <Image className={'header-logo'} src={BMDBImage}/>
+              </Navbar.Header>
+              <Nav className={"header-pullright"}>
+                <NavItem>{"POPULAR"}</NavItem>
+                <NavItem>{"FAVORITES"}</NavItem>
+              </Nav>
+              </Navbar>
+               <div>
+               {this._sortRating}
+               {this._sortYear}
+               </div>
             </div>
         );
     }
---
+++ src/components/bmdbHeader.js
@@ -0,0 +1,27 @@
+/*BMDB Search Header*/
+
+import React, { Component } from 'react';
+import { Navbar, Image, NavItem, Nav } from 'react-bootstrap';
+import BMDBImage from '../img/logo.png';
+
+class BMDBHeader extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+    render() {
+        return (
+            <Navbar>
+	            <Navbar.Header>
+	            <Image className={'header-logo'} src={BMDBImage}/>
+	            </Navbar.Header>
+	            <Nav className={"header-pullright"}>
+	            <NavItem>{"POPULAR"}</NavItem>
+				<NavItem>{"FAVORITES"}</NavItem>
+				</Nav>
+            </Navbar>
+        )
+    }
+}
+
+export default BMDBHeader;
