dbdc47dd378a5ae27d8fdcd3ba47383d5b625969
shashank.keshava <shashank.keshava@bookmyshow.com>
2017-08-29 18:25:54 +0530

    Redux app

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
@@ -6,4 +6,21 @@ import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+registerServiceWorker();*/
+
+
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import App from './components/Apps'
+
+let store = createStore(todoApp)
+
+render(
+    <Provider store={store}>
+    <App />
+  </Provider>,
+    document.getElementById('root')
+)
